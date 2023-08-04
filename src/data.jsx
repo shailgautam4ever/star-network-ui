d3957970a52b47149621c80e366dda05711e1274
Ravi <raysk7161@gmail.com>
2023-08-04 22:54:30 +0530

    mock data

---
+++ src/data.jsx
@@ -0,0 +1,19 @@
+
+// export const navLeftItems = ["Movies", "TV Shows", "Documantries" ]
+export const navLeftItems = [{
+  name:"Movies",
+  url: "/movies"
+},{
+  name:"TV Shows",
+  url: "/tv-shows"
+},{
+  name:"Documantries",
+  url: "/documantries"
+}]
+export const sliderItems = ['/assets/starwars.jpeg','/assets/oppenheimer.jpeg','/assets/frozen2.jpeg','/assets/starwars.jpeg','/assets/starwars.jpeg','/assets/starwars.jpeg','/assets/kong.png']
+export const movie = {
+  title:"STAR WARS",
+  tagline:"THE RISE OF SKYWALKER",
+  description:"The surviving members of star wars begins a new war against the people who are bad and are likely to win this battle to get to know more visits theaters.",
+  rating:4
+}
